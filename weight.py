0f1a728f05469c63a0099601bd251994ac8c6896
sortme tetsuya <sortme@sortme-no-MacBook-Pro.local>
2017-05-21 03:43:07 +0900

    fix

--- weight.py
+++ weight.py
@@ -3,7 +3,7 @@ import xlrd
 import numpy as np
 import os
 from matplotlib import pyplot as plt
-book = xlrd.open_workbook('/Users/sortme/Documents/統計記録.xls')
+book = xlrd.open_workbook('$HOME/統計記録.xls')
 sheet = book.sheet_by_index(1)
 nrows = sheet.nrows
 data = np.zeros(2*nrows).reshape((nrows,2))
@@ -16,4 +16,4 @@ for col in [0, 1]:
 		data[row,col] = sheet.cell(row,col).value
 weight = data[:,1]
 plt.plot(date, weight, label="weight transition")
-plt.savefig("/Users/sortme/Desktop/weight.png")
+plt.savefig("$HOME/Desktop/weight.png")
