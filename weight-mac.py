3a8240db468f18f0a7908bcd3434b61db6c1288d
sortme tetsuya <sortme@sortme-no-MacBook-Pro.local>
2017-07-30 13:51:55 +0900

    add grid

--- weight-mac.py
+++ weight-mac.py
@@ -2,10 +2,8 @@
 import xlrd
 import numpy as np
 import os
-import matplotlib
-matplotlib.use('Agg')
 from matplotlib import pyplot as plt
-book = xlrd.open_workbook('$HOME/weight.xls')
+book = xlrd.open_workbook('/Users/sortme/Documents/統計記録.xls')
 sheet = book.sheet_by_index(1)
 nrows = sheet.nrows
 data = np.zeros(2*nrows).reshape((nrows,2))
@@ -17,9 +15,8 @@ for col in [0, 1]:
 			date.append(d)
 		data[row,col] = sheet.cell(row,col).value
 weight = data[:,1]
-plt.figure(figsize=(6,4))
 plt.xticks(rotation =12)
-plt.plot(date, weight, label=date[nrows-1])
+plt.plot(date, weight, label="weight transition")
 plt.legend()
-plt.grid()
-plt.savefig("$HOME/weight.png")
+#plt.savefig("/Users/sortme/weight.png")
+plt.show()
